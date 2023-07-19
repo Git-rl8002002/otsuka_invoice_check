115e9ba8dc1d3e25b46c74fdfd2c4945b19b7c82
unknown <Jason_Hung@jason_otsuka.otsukatw.corp>
2023-07-19 09:36:49 +0800

    add files

---
+++ config/config.py
@@ -0,0 +1,12 @@
+# -*- coding: UTF-8 -*-
+
+# Author   : Jason Hung
+# Build    : 20230714
+# update   : 20230717
+# Function : check invoice email attachments errors
+
+##############
+# file path
+##############
+data_dir = {"win2019":"C:/Jason_python/invoice_check/file"}
+
